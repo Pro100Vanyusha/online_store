48cce3873872c4ec4207528496617a14a3298afd
Ivan <muzikaivan2018@gmail.com>
2024-04-13 21:40:21 +0300

    додавання рейтингу але не працюючого

--- server/controllers/ratingController.js
+++ server/controllers/ratingController.js
@@ -1,16 +1,16 @@
-const {Rating, sequelize} = require('../models/models');
+const { Rating, sequelize } = require('../models/models');
 const ApiError = require('../error/ApiError');
 
 class RatingController {
     async upsertRating(req, res, next) {
         try {
-            const {userId, itemId, rating} = req.body;
-            const existingRating = await Rating.findOne({where: {userId, itemId}});
+            const { userId, itemId, rating } = req.body;
+            const existingRating = await Rating.findOne({ where: { userId, itemId } });
             if (existingRating) {
                 existingRating.rating = rating;
                 await existingRating.save();
             } else {
-                await Rating.create({userId, itemId, rating});
+                await Rating.create({ userId, itemId, rating });
             }
             res.status(200).send("Rating updated successfully");
         } catch (error) {
@@ -20,13 +20,13 @@ class RatingController {
 
     async getAverageRating(req, res, next) {
         try {
-            const {itemId} = req.params;
+            const { itemId } = req.params;
             const avgRating = await Rating.findAll({
-                where: {itemId},
+                where: { itemId },
                 attributes: [[sequelize.fn('avg', sequelize.col('rating')), 'averageRating']],
                 raw: true,
             });
-            res.json({averageRating: avgRating[0].averageRating || 0});
+            res.json({ averageRating: avgRating[0].averageRating || 0 });
         } catch (error) {
             next(error);
         }
